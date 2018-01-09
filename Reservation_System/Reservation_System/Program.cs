a4863a91e94c999a05b5724c0fc102f9325a7620
Ville Kokkarinen <kokkarinen.ville@gmail.com>
2018-01-09 08:51:03 +0200

    Empty commit #1
    asd

---
+++ Reservation_System/Reservation_System/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reservation_System
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new User_InterFace());
+
+        }
+    }
+}
---
+++ Reservation_System/Reservation_System/User_InterFace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reservation_System
+{
+    public partial class User_InterFace : Form
+    {
+        public User_InterFace()
+        {
+            InitializeComponent();
+        }
+
+        private void User_InterFace_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
